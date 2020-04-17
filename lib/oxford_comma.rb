592c578decda19e7e3abdf4ee3fcba32610978ed
Marcus Lau <lau.marcus76@gmail.com>
2020-04-17 02:17:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
 
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    str = array[0..-2].join(', ') 
+    str.concat(", and #{array[-1]}")
+    return str
+  end
 end
