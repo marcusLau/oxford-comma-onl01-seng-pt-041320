981e0f74ef1df0317acef1e260b974cb880f35ec
Marcus Lau <lau.marcus76@gmail.com>
2020-04-17 02:14:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,21 +2,5 @@ require 'pry'
 
 
 def oxford_comma(array)
-  case array.size
   
-  when array.size == 1 
-    return array.join
-  when array.size == 2
-    return "#{array[0]} and #{array[1]}"
-  when array.size >= 3
-    oxford_comma = ""
-    for i in 0..array.size-1
-      if i == array.size-1
-        oxford_comma.concat("and #{array[i]}")
-      else 
-        oxford_comma.concat("#{array[i]}, ")
-      end
-    end
-    return oxford_comma
-  end
 end
