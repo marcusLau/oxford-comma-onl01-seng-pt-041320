f88b9942c510679be6f0ec2d0151f9def465c023
Marcus Lau <lau.marcus76@gmail.com>
2020-04-17 01:54:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,4 +7,7 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   when array.size >= 3
     oxford_comma = ""
+    for i in 0..array.size-1
+      if i == array.size-1
+        oxford_comma.concat("and #{array[i]")
 end
